325fef22777fe855cff67c744c08eddee8b944b8
Paz Jaldety <paz@coperato.local>
2018-12-12 12:27:44 +0200

    Finished lecture 128

--- imports/ui/Login.test.js
+++ imports/ui/Login.test.js
@@ -8,14 +8,23 @@ import {spy} from 'sinon';
 var chai = require("chai");
 var sinonChai = require("sinon-chai");
 chai.use(sinonChai);
-
 configure({ adapter: new Adapter() });
+
+import {Login} from './Login';
+
 
 if(Meteor.isClient){
   describe('Login' , function () {
     it('should show error messages' , function() {
       const err = 'This is not working';
-      const wrapper = mount(<)
+      const wrapper = mount(<Login loginWithPassword={() => {}}/>);
+      wrapper.setState({error : err});
+
+      const paragraph = wrapper.find('p').text();
+      expect(paragraph).to.equal(err);
+
+      wrapper.setState({error: ''});
+      expect(wrapper.find('p').length).to.equal(0);
     });
   });
 }
