3339d93361accfa8b6a47519b95ce205107d8e8f
Paz Jaldety <paz@coperato.local>
2019-01-08 10:19:55 +0200

    Added test fixtures

--- imports/fixtures/fixtures.js
+++ imports/fixtures/fixtures.js
@@ -3,13 +3,13 @@ export const notes = [
     _id: 'testNoteId1',
     title: 'Test title 1',
     body: '',
-    updatedAt: 0,
+    updatedAt: 1546787205912,
     userId: 'testUserId1'
   },{
     _id: 'testNoteId2',
-    title: 'Test title 2',
+    title: '',
     body: 'Test body number 2',
-    updatedAt: 0,
+    updatedAt: 1546787205912,
     userId: 'testUserId2'
   }
 ];
--- imports/ui/NoteListItem.test.js
+++ imports/ui/NoteListItem.test.js
@@ -3,23 +3,40 @@ const expect = require('chai').expect;
 import { Meteor } from 'meteor/meteor';
 import {mount} from 'enzyme';
 
-import NoteListItem from './NoteListItem';
+import {spy} from 'sinon';
+var chai = require("chai");
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+
+import {NoteListItem} from './NoteListItem';
+
+import {notes} from '../fixtures/fixtures';
 
 
 if(Meteor.isClient){
   describe('NoteListItem' , function () {
+    let Session;
+    beforeEach(() => {
+      Session = {
+        set: spy()
+      };
+    });
+
     it('should render title and timestamp' , () => {
-      const title = 'Test title';
-      const updatedAt = 1546787205912;
-      const wrapper =mount(<NoteListItem note={{title, updatedAt} }/>);
-      expect(wrapper.find('h5').text()).to.equal(title);
+      const wrapper = mount(<NoteListItem note={notes[0] } Session={Session}/>);
+      expect(wrapper.find('h5').text()).to.equal(notes[0].title);
       expect(wrapper.find('p').text()).to.equal('06/01/2019');
     });
     it('should set default title if no title set' ,() => {
-      const updatedAt = 1546787205912;
-      const wrapper =mount(<NoteListItem note={{ updatedAt} }/>);
+      const wrapper = mount(<NoteListItem note={notes[1]} Session={Session}/>);
       expect(wrapper.find('h5').text()).to.equal('Untitled note');
       expect(wrapper.find('p').text()).to.equal('06/01/2019');
+    });
+
+    it('should call set on click' , () => {
+      const wrapper = mount(<NoteListItem note={notes[1]} Session={Session}/>);
+      wrapper.find('div').simulate('click');
+      expect(Session.set).to.have.been.calledWith('selectedNoteId' , notes[1]._id);
     });
   });
 }
